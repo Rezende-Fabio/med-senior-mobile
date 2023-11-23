e71169bbed5697faf5bfae0bdc76e393199591c2
Rezende-Fabio <rezendefabio337@gmail.com>
2023-11-23 00:05:51 -0300

    catching geolocation

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
-import 'pages/start_page.dart';
+import 'pages/start_page/start_page.dart';
 import './pages/login_page.dart';
 import 'pages/register_page/register_page.dart';
 import './pages/home_page.dart';
